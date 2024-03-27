42bb30cd4b9dc62396e2e760db808a5883e860d1
PenguinSatanJr <screemerss@gmail.com>
2024-03-27 15:21:57 +0400

    init db config updated

--- postgres/init-dbs.sh
+++ postgres/init-dbs.sh
@@ -5,7 +5,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER penguin WITH PASSWORD 'penguin';
     CREATE DATABASE recipes;
     GRANT ALL PRIVILEGES ON DATABASE recipes TO penguin;
-    CREATE USER "recipes-test" WITH PASSWORD 'recipes-test';
+    CREATE USER "penguin-test" WITH PASSWORD 'penguin-test';
     CREATE DATABASE "recipes-test";
-    GRANT ALL PRIVILEGES ON DATABASE "recipes-test" TO "recipec-test";
+    GRANT ALL PRIVILEGES ON DATABASE "recipes-test" TO "penguin-test";
 EOSQL
